4b03de09b17d62e2c29a11dd20c597b5f435d2b3
Nasrin Chakkingal <nasrinshameer17@gmail.com>
2025-09-16 08:57:52 +0530

    redirects

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(frontendPath));
 
   // ✅ React Router fallback
-  app.get("*", (req, res) => {
+  app.get("(/^\/(?!api).*/)", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
