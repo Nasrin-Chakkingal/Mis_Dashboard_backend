cdd55bf80146b04a430d64c1d0e445f409f769bc
Nasrin Chakkingal <nasrinshameer17@gmail.com>
2025-09-16 09:48:59 +0530

    re

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 
 import salesRoutes from "./routes/sales.routes.js";
@@ -30,6 +32,20 @@ app.use(notFound);
 app.use(errorHandler);
 
 app.get("/healthz", (_, res) => res.send("ok"));
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === 'production') {
+  const frontendPath = path.join(__dirname, '../MIS_Dashboard/dist');
+  app.use(express.static(frontendPath));
+  app.get('/*', (req, res) => {
+  res.sendFile(path.join(frontendPath, '/index.html'));
+});
+}
+
 
 
 export default app;
