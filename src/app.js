ae95fe3cb34c471e63e8f0213782aaf9f9ca70e3
Nasrin Chakkingal <nasrinshameer17@gmail.com>
2025-09-16 10:05:00 +0530

    redirects

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 import express from "express";
 import cors from "cors";
-import path from 'path';
-import { fileURLToPath } from 'url';
-
 
 import salesRoutes from "./routes/sales.routes.js";
 import customerRoutes from "./routes/customer.routes.js";
@@ -12,7 +9,6 @@ import inventoryRoutes from "./routes/inventory.routes.js";
 
 import notFound from "./middleware/notfound.js";
 import errorHandler from "./middleware/errorhandler.js";
-
 
 const app = express();
 
@@ -26,26 +22,10 @@ app.use("/api", filterRoutes);
 app.use("/api", summaryRoutes);
 app.use("/api", inventoryRoutes);
 
-
-
-app.use(notFound);
-app.use(errorHandler);
-
 app.get("/healthz", (_, res) => res.send("ok"));
 
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-if (process.env.NODE_ENV === 'production') {
-  const frontendPath = path.join(__dirname, '../MIS_Dashboard/dist');
-  app.use(express.static(frontendPath));
-  app.get('/*', (req, res) => {
-  res.sendFile(path.join(frontendPath, '/index.html'));
-});
-}
-
-
+// Error handlers
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
